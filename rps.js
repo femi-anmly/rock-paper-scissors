3837317edb5a1105245b4b4d2c6a47c427473dfc
femi-anmly <femiogun9590@gmail.com>
2024-05-27 15:50:24 -0400

    Create playGame function
    Create a playGame function where playRound is
    called five times and the winner is announced.

--- rps.js
+++ rps.js
@@ -41,28 +41,31 @@ function getHumanChoice() {
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
+    let humanScore = 0;
+    let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice();
     humanChoice = humanChoice.substring(0,1) + humanChoice.slice(1).toLowerCase();
-
+    computerChoice = getComputerChoice();
+    console.log(humanChoice);
+    console.log(computerChoice);
     if (humanChoice === "Rock" && computerChoice === "Scissors")
     {
         humanScore++;
-        console.log("You lose! " + humanChoice + " beats " + computerChoice);
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
     }
 
     else if (humanChoice === "Scissors" && computerChoice === "Paper")
     {
         humanScore++;
-        console.log("You lose! " + humanChoice + " beats " + computerChoice);
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
     }
 
     else if (humanChoice === "Paper" && computerChoice === "Rock")
     {
         humanScore++;
-        console.log("You lose! " + humanChoice + " beats " + computerChoice);
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
     }
 
     else if (computerChoice === "Rock" && humanChoice === "Scissors")
@@ -89,7 +92,34 @@ function playRound(humanChoice, computerChoice) {
 }
 
 const humanSelection = getHumanChoice();
-console.log(humanSelection);
 const computerSelection = getComputerChoice();
-console.log(computerSelection);
-playRound(humanSelection, computerSelection);
+
+
+
+function playGame() {
+    let gameCount = 0;
+    humanScore = 0;
+    computerScore = 0;
+
+    while (gameCount < 5)
+    {
+        playRound(humanSelection, computerSelection);
+        gameCount++;
+        
+    }
+
+    if (humanScore > computerScore)
+    {
+        console.log("You won! The score was " + humanScore + " - " + computerScore);
+    }
+
+    else if (humanScore < computerScore) {
+        console.log("You lost! The score was " + humanScore + " - " + computerScore);
+    }
+
+    else {
+        console.log("You tied! The score was " + humanScore + " - " + computerScore);
+    }
+}
+
+playGame();
