479312ed367c3a2771595521d3a8efbf2c140f7f
femi-anmly <femiogun9590@gmail.com>
2024-05-27 16:12:33 -0400

    Clean up space by adding playRound inside playGame

--- rps.js
+++ rps.js
@@ -44,65 +44,69 @@ function getHumanChoice() {
     let humanScore = 0;
     let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanChoice();
-    humanChoice = humanChoice.substring(0,1) + humanChoice.slice(1).toLowerCase();
-    computerChoice = getComputerChoice();
-    console.log(humanChoice);
-    console.log(computerChoice);
-    if (humanChoice === "Rock" && computerChoice === "Scissors")
-    {
-        humanScore++;
-        console.log("You win! " + humanChoice + " beats " + computerChoice);
-    }
 
-    else if (humanChoice === "Scissors" && computerChoice === "Paper")
-    {
-        humanScore++;
-        console.log("You win! " + humanChoice + " beats " + computerChoice);
-    }
 
-    else if (humanChoice === "Paper" && computerChoice === "Rock")
-    {
-        humanScore++;
-        console.log("You win! " + humanChoice + " beats " + computerChoice);
-    }
 
-    else if (computerChoice === "Rock" && humanChoice === "Scissors")
-    {
-        computerScore++;
-        console.log("You lose! " + computerChoice + " beats " + humanChoice);
-    }
-
-    else if (computerChoice === "Scissors" && humanChoice === "Paper")
-    {
-        computerScore++;
-        console.log("You lose! " + computerChoice + " beats " + humanChoice);
-    }
-
-    else if (computerChoice === "Paper" && humanChoice === "Rock")
-    {
-        computerScore++;
-        console.log("You lose! " + computerChoice + " beats " + humanChoice);
-    }
-
-    else {
-        console.log("You tied!");
-    }   
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
+ const humanSelection = getHumanChoice();
+ const computerSelection = getComputerChoice();
 
 
 function playGame() {
     let gameCount = 0;
     humanScore = 0;
     computerScore = 0;
+    
 
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = getHumanChoice();
+        humanChoice = humanChoice.substring(0,1) + humanChoice.slice(1).toLowerCase();
+        computerChoice = getComputerChoice();
+        console.log(humanChoice);
+        console.log(computerChoice);
+        if (humanChoice === "Rock" && computerChoice === "Scissors")
+        {
+            humanScore++;
+            console.log("You win! " + humanChoice + " beats " + computerChoice);
+        }
+    
+        else if (humanChoice === "Scissors" && computerChoice === "Paper")
+        {
+            humanScore++;
+            console.log("You win! " + humanChoice + " beats " + computerChoice);
+        }
+    
+        else if (humanChoice === "Paper" && computerChoice === "Rock")
+        {
+            humanScore++;
+            console.log("You win! " + humanChoice + " beats " + computerChoice);
+        }
+    
+        else if (computerChoice === "Rock" && humanChoice === "Scissors")
+        {
+            computerScore++;
+            console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        }
+    
+        else if (computerChoice === "Scissors" && humanChoice === "Paper")
+        {
+            computerScore++;
+            console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        }
+    
+        else if (computerChoice === "Paper" && humanChoice === "Rock")
+        {
+            computerScore++;
+            console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        }
+    
+        else {
+            console.log("You tied!");
+        }   
+    }
+    
     while (gameCount < 5)
-    {
+    {   
+        
         playRound(humanSelection, computerSelection);
         gameCount++;
         
